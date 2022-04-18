e15ffe52813559cc480d4dc785c38276e4737ae8
Rozov Alexander <a_rozov@internet.ru>
2022-04-19 00:22:21 +0300

    Урок 12. Домашнее задание выполнено

--- TODO_notes/TODO_notes/settings/base.py
+++ TODO_notes/TODO_notes/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lx(t89p4ghsv_k+z-@q&2hlr#qja0psu5&q2uu4eq%g2gzfa^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,12 +40,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'graphene_django',
     'drf_yasg',
     'corsheaders',
     'usersapp',
-    'TODOapp',
-    
+    'TODOapp',    
 ]
 
 REST_FRAMEWORK = {
@@ -131,12 +129,7 @@ WSGI_APPLICATION = 'TODO_notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
